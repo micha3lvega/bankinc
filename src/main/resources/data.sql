15096deff142e004be6328d66d8b1708981a4bfb
Michael Vega <micha3lvega@gmail.com>
2024-05-30 23:12:10 -0500

    Configuracion del insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO `products` (`product_id`,`name`,`prefix`,`state`) VALUES (1,'American Express','52090190',1);
-INSERT INTO `products` (`product_id`,`name`,`prefix`,`state`) VALUES (2,'Dinners Club','63245612',1);
-INSERT INTO `products` (`product_id`,`name`,`prefix`,`state`) VALUES (3,'Master Card','63245612',1);
-INSERT INTO `products` (`product_id`,`name`,`prefix`,`state`) VALUES (4,'Visa','46790123',1);
+MERGE INTO products (product_id, name, prefix, state) VALUES (1, 'American Express', '52090190', 1);
+MERGE INTO products (product_id, name, prefix, state) VALUES (2, 'Dinners Club', '63245612', 1);
+MERGE INTO products (product_id, name, prefix, state) VALUES (3, 'Master Card', '63245612', 1);
+MERGE INTO products (product_id, name, prefix, state) VALUES (4, 'Visa', '46790123', 1);
